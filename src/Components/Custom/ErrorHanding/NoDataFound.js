1d9f265e3ae227e6fe087e5c715aa5d86a423ee2
karuna kashyap <kashyapkaruna60@gmail.com>
2024-01-03 22:37:18 +0530

    add No Data Found Component

---
+++ src/Components/Custom/ErrorHanding/NoDataFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ReactComponent as WarningBlack } from '../../../assets/Icons/warningBlack.svg';
+
+const NoDataFound = () => {
+    return (
+        <div>
+            <div className=' w-[700px] bg-gray-100 border flex rounded  p-2 mt-6'>
+                <div className=' mt-[2px]'><WarningBlack /></div>
+                <span className=' ml-3 text-gray-500'>No Data Found</span>
+            </div>
+        </div>
+    )
+}
+
+export default NoDataFound
