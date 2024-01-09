3889748b39b0d5860e0825021b938b2a05f12424
karuna kashyap <kashyapkaruna60@gmail.com>
2024-01-09 13:56:02 +0530

    change breadcrumbs

--- src/Components/UI/BreadCrumbs/BreadCrumbs.js
+++ src/Components/UI/BreadCrumbs/BreadCrumbs.js
@@ -1,30 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { ReactComponent as ChevronRight } from '../../../assets/Icons/ChevronRight.svg';
 
-const BreadCrumbs = () => {
+const Breadcrumbs = () => {
+    const location = useLocation();
+    const currentUrl = location.pathname.split('/').filter((x) => x);
     return (
-        <div className=' mt-5'>
-            <nav aria-label="Breadcrumb">
-                <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-                    <li className="inline-flex items-center">
-                        <Link to="/overview"  className="inline-flex items-center text-sm font-medium text-gray-400 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white" >
-                            OVERVIEW
-                        </Link>
-                    </li>
-                    <li>
-                        <div className="flex items-center">
-                            <svg className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
-                            </svg>
-                            <Link to="/overview/payouts" className="ms-1 text-sm font-medium text-gray-400 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">PAYOUTS</Link>
-                        </div>
-                    </li>
-                </ol>
-            </nav>
-
+        <div className='flex text-gray-500 text-lg justify-center mt-5'>
+            <Link to="/">overview</Link>
+            {currentUrl.map((pathname, index) => {
+                console.log(`/${currentUrl.slice(0, index + 1)}`)
+                const routeTo = `/${currentUrl.slice(0, index + 1).join('/')}`;
+                const isLast = index === currentUrl.length - 1;
+                return (
+                    <div key={pathname}  >
+                        <div className=' flex'>
+                       <div className=' mt-1'> <ChevronRight/></div>
+                       {isLast ? (<div className=' '>{pathname}</div>) : (<Link to={routeTo}>{pathname}</Link>)}
+                       </div>
+                    </div>
+                );
+            })}
         </div>
+    );
+};
 
-    )
-}
-
-export default BreadCrumbs
+export default Breadcrumbs;
