50d230018627101e971a9bd7e8a91a2e7ccff302
karuna kashyap <kashyapkaruna60@gmail.com>
2024-01-15 10:28:53 +0530

    add changes in pnl form

--- src/Components/Form/Input/FormInput.js
+++ src/Components/Form/Input/FormInput.js
@@ -6,12 +6,22 @@ const FormInput = ({ titleName, fontSize, type, source, value }) => {
         <div>
             <form action="">
                 <div class="grid grid-cols-2">
-                    <div><div className='flex space-x-1 mt-1'><label for="name" className={` text-gray-400 mb-2  text-${fontSize}`}>{titleName}</label>
-                        <div className=' mt-1'><ExclamationCircle /></div></div></div>
-                    <div><div className='relative right-4'> {source == "input" ?
-                        <input type={type} name="number" id="number" step=".0" placeholder='Type Here...' required class="p-2 border rounded py-2 px-4 w-72 text-gray-400" />
-                        : <input type={type} value={value} name="number" id="number" step=".0" disabled placeholder='Type Here...' required class="p-2 border rounded py-2 px-4 w-72 text-gray-400" />
-                    }</div></div>
+                    <div className='w-[400px]'>
+                        <div className='flex space-x-1 mt-1'>
+                            <label for="name" className={` text-gray-400 mb-2  text-${fontSize}`}>{titleName}</label>
+                            <div className=' mt-1'>
+                                <ExclamationCircle />
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='relative left-10'>
+                            {source == "input" ?
+                                <input type={type} name="number" id="number" step=".0" placeholder='Type Here...' required class="p-2 border rounded py-2 px-4 w-72 text-gray-400" />
+                                : <input type={type} value={value} name="number" id="number" step=".0" disabled placeholder='Type Here...' required class="p-2 border rounded py-2 px-4 w-72 text-gray-400" />
+                            }
+                        </div>
+                    </div>
                 </div>
             </form>
         </div>
--- src/Pages/PnlForm.js
+++ src/Pages/PnlForm.js
@@ -4,15 +4,21 @@ import jsonData from '../assets/Jsons/Data.json'
 import HeaderSection from '../Components/UI/HeaderSection/HeaderSection';
 
 const PnlForm = () => {
-    const [showDownData, setShowDownData] = useState(false);
-    const showJsonData = () => {
-        setShowDownData(true)
+    const [showDownData, setShowDownData] = useState(null);
+    const showJsonData = (indexValue) => {
+        setShowDownData(indexValue === showDownData ? null : indexValue)
         setChevronup(false)
         setChevronDown(true)
     }
-    const [showDownData1, setShowDownData1] = useState(false);
-    const showJsonData1 = () => {
-        setShowDownData1(true);
+    const [showDownData1, setShowDownData1] = useState(null);
+    const showJsonData1 = (indexValue) => {
+        setShowDownData1(indexValue === showDownData ? null : indexValue);
+        setChevronup(false)
+        setChevronDown(true)
+    }
+    const [showDownData2, setShowDownData2] = useState(null);
+    const showJsonData2 = (indexValue) => {
+        setShowDownData2(indexValue === showDownData ? null : indexValue);
         setChevronup(false)
         setChevronDown(true)
     }
@@ -21,18 +27,17 @@ const PnlForm = () => {
 
     return (
         <div>
-            <div className='border-2 p-4 w-[600px] ml-[500px] mb-32'>
+            <div className='border-2 p-4 w-[750px] ml-[350px] mb-32'>
 
                 {jsonData.map((field, index) => {
-
                     return (
                         <div>
                             {(() => {
                                 if (field.title != null) {
-                                    return <div><HeaderSection chevronDown={chevronDown} chevronup={chevronup} showJsonData={showJsonData}  title={field?.title} /></div>;
+                                    return <div><HeaderSection chevronDown={chevronDown} chevronup={chevronup} showJsonData={() => showJsonData(index)} title={field?.title} /></div>;
                                 }
                             })()}
-                            {showDownData &&
+                            {showDownData === index &&
                                 <div>{field?.form_fields?.map((formField, index) => {
                                     return (
                                         <div className=' mt-2'><FormInput titleName={formField?.element_name} type={formField.data_type} source={formField.source} value={formField.value} /></div>
@@ -42,9 +47,9 @@ const PnlForm = () => {
                                         return (<div>{
                                             (() => {
                                                 if (subField?.title != null) {
-                                                    return <div>
-                                                        <HeaderSection chevronDown={chevronDown} chevronup={chevronup} title={subField?.title}  showJsonData={showJsonData1} />
-                                                        {showDownData1 && <div> {subField?.form_fields?.map((formField, index) => {
+                                                    return <div className=' ml-10'>
+                                                        <HeaderSection chevronDown={chevronDown} chevronup={chevronup} title={subField?.title} showJsonData={() => showJsonData1(index)} />
+                                                        {showDownData1 === index && <div> {subField?.form_fields?.map((formField, index) => {
                                                             return (
                                                                 <div className='mt-2'><FormInput titleName={formField?.element_name} type={formField.data_type} source={formField.source} value={formField.value} /></div>
                                                             )
@@ -55,18 +60,22 @@ const PnlForm = () => {
                                                 }
                                             })()}
                                             {subField?.sub_fields?.map((subField2, index) => {
-                                                return (<div>
+                                                return (<div className='ml-16'>
                                                     {(() => {
                                                         if (subField2.title != null) {
                                                             return <div>
-                                                                <HeaderSection chevronDown={chevronDown} chevronup={chevronup}  showJsonData={showJsonData1} title={subField2.title} />
-                                                                {showDownData1 && <div> {subField2?.form_fields?.map((formField, index) => {
+                                                                <HeaderSection chevronDown={chevronDown} chevronup={chevronup} showJsonData={() => showJsonData2(index)} title={subField2.title} />
+                                                                {showDownData2 === index && <div> {subField2?.form_fields?.map((formField, index) => {
                                                                     return (
-                                                                        <div className='mt-2'><FormInput titleName={formField?.element_name} type={formField.data_type} source={formField.source} value={formField.value} /></div>
+                                                                        <div>
+                                                                            <div className='mt-2'><FormInput titleName={formField?.element_name} type={formField.data_type} source={formField.source} value={formField.value} /></div>
+                                                                        </div>
                                                                     )
                                                                 })}
                                                                 </div>}
-                                                            </div>;
+
+                                                            </div>
+
                                                         }
                                                     })()}
 
