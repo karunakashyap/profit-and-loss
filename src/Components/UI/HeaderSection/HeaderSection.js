362a6f061166cb521246acb4898fad051a7b8bce
karuna kashyap <kashyapkaruna60@gmail.com>
2024-01-16 17:28:02 +0530

    pnlForm field input

--- src/Components/UI/HeaderSection/HeaderSection.js
+++ src/Components/UI/HeaderSection/HeaderSection.js
@@ -5,15 +5,14 @@ import { ReactComponent as ChevronDown } from '../../../assets/Icons/ChevronDown
 
 
 
-const HeaderSection = ({ title, showJsonData, chevronDown, chevronup,hideDownData }) => {
+const HeaderSection = ({ title, showJsonData, chevron }) => {
     return (
         <div>
             <div className='flex  mt-8 space-x-3 p-1'>
                 <div className=' p-1 bg-gray-300 border rounded'><MenuBar /></div>
                 <h1 className=' font-bold mt-1'>{title}</h1>
                 <div className=' mt-[6px]' onClick={showJsonData}>
-                    {chevronup && <ChevronUp />}
-                    {chevronDown && <ChevronDown />}
+                    {chevron}
                 </div>
             </div>
         </div>
--- src/Pages/PnlForm.js
+++ src/Pages/PnlForm.js
@@ -1,40 +1,23 @@
 import React, { useState } from 'react'
 import FormInput from '../Components/Form/Input/FormInput'
-import jsonData from '../assets/Jsons/Data.json'
 import HeaderSection from '../Components/UI/HeaderSection/HeaderSection';
-
-const PnlForm = () => {
+import { ReactComponent as ChevronDown } from '../assets/Icons/ChevronDown.svg';
+import { ReactComponent as ChevronUp } from '../assets/Icons/ChevronUp.svg';
+const PnlForm = ({ jsonData }) => {
     const [showDownData, setShowDownData] = useState(null);
     const showJsonData = (indexValue) => {
         setShowDownData(indexValue === showDownData ? null : indexValue)
-        setChevronup(false)
-        setChevronDown(true)
     }
-    const [showDownData1, setShowDownData1] = useState(null);
-    const showJsonData1 = (indexValue) => {
-        setShowDownData1(indexValue === showDownData ? null : indexValue);
-        setChevronup(false)
-        setChevronDown(true)
-    }
-    const [showDownData2, setShowDownData2] = useState(null);
-    const showJsonData2 = (indexValue) => {
-        setShowDownData2(indexValue === showDownData ? null : indexValue);
-        setChevronup(false)
-        setChevronDown(true)
-    }
-    const [chevronup, setChevronup] = useState(true);
-    const [chevronDown, setChevronDown] = useState(false)
-
     return (
         <div>
-            <div className='border-2 p-4 w-[750px] ml-[350px] mb-32'>
+            <div >
 
                 {jsonData.map((field, index) => {
                     return (
                         <div>
                             {(() => {
                                 if (field.title != null) {
-                                    return <div><HeaderSection chevronDown={chevronDown} chevronup={chevronup} showJsonData={() => showJsonData(index)} title={field?.title} /></div>;
+                                    return <div><HeaderSection chevron={showDownData === index ? <ChevronUp /> : <ChevronDown />} showJsonData={() => showJsonData(index)} title={field?.title} /></div>;
                                 }
                             })()}
                             {showDownData === index &&
@@ -43,47 +26,9 @@ const PnlForm = () => {
                                         <div className=' mt-2'><FormInput titleName={formField?.element_name} type={formField.data_type} source={formField.source} value={formField.value} /></div>
                                     )
                                 })}
-                                    {field?.sub_fields?.map((subField, index) => {
-                                        return (<div>{
-                                            (() => {
-                                                if (subField?.title != null) {
-                                                    return <div className=' ml-10'>
-                                                        <HeaderSection chevronDown={chevronDown} chevronup={chevronup} title={subField?.title} showJsonData={() => showJsonData1(index)} />
-                                                        {showDownData1 === index && <div> {subField?.form_fields?.map((formField, index) => {
-                                                            return (
-                                                                <div className='mt-2'><FormInput titleName={formField?.element_name} type={formField.data_type} source={formField.source} value={formField.value} /></div>
-                                                            )
-                                                        })}
-                                                        </div>}
-
-                                                    </div>
-                                                }
-                                            })()}
-                                            {subField?.sub_fields?.map((subField2, index) => {
-                                                return (<div className='ml-16'>
-                                                    {(() => {
-                                                        if (subField2.title != null) {
-                                                            return <div>
-                                                                <HeaderSection chevronDown={chevronDown} chevronup={chevronup} showJsonData={() => showJsonData2(index)} title={subField2.title} />
-                                                                {showDownData2 === index && <div> {subField2?.form_fields?.map((formField, index) => {
-                                                                    return (
-                                                                        <div>
-                                                                            <div className='mt-2'><FormInput titleName={formField?.element_name} type={formField.data_type} source={formField.source} value={formField.value} /></div>
-                                                                        </div>
-                                                                    )
-                                                                })}
-                                                                </div>}
-
-                                                            </div>
-
-                                                        }
-                                                    })()}
-
-                                                </div>)
-                                            })}
-                                        </div>)
-                                    })}
-                                </div>}
+                                    <div className=' mt-10'> {field.sub_fields && <PnlForm jsonData={field.sub_fields} />}</div>
+                                </div>
+                            }
                         </div>
                     )
                 })}
