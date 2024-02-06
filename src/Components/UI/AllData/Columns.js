d4c34a8043a726a325d3ab7882e67b9ba41690a4
karuna kashyap <kashyapkaruna60@gmail.com>
2024-02-06 23:28:03 +0530

    add some changes in allData Table component

--- src/Components/UI/AllData/Columns.js
+++ src/Components/UI/AllData/Columns.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import TableData from '../../../assets/Jsons/TableData.json';
-const Columns = ({ Data, font }) => {
+const Columns = ({ Data, font,border }) => {
     return (
         <div >
             {Data?.map((title) => {
                 return <div className={`${font}`}>
-                    <h1 className='border-2 w-full font-bold'>{title.title}</h1>
-                    <div>{title.sub_fields && <Columns Data={title.sub_fields} font={'text-lg'} />}</div>
+                    <h1 className={`border w-full font-bold ${border} `}>{title.title}</h1>
+                    <div>{title.sub_fields && <Columns Data={title.sub_fields} font={'text-lg'} border={'border-l-2 border-r-2'}/>}</div>
                     <div> {title.form_fields && <Columns Data={title.form_fields} font={'text-sm'} />}</div>
-                    <div className='flex'>
-                        <h1 className='border w-44'>{title.element_name}</h1>
+                    <div className=' border-2 flex'>
+                        <h1 className=' w-44'>{title.element_name}</h1>
                         <div className='flex'>{TableData.column_data.map((item) => (
-                            item.form_field_id === title.form_field_id ? item.data.map((v) => (<div className='border w-28 p-2'>{v.value}</div>)) : ''
+                            item.form_field_id === title.form_field_id ? item.data.map((v) => (<div className=' border-l-2 w-28 p-2'>{v.value}</div>)) : ''
                         ))}
                         </div>
                     </div>
--- src/Pages/AllData.js
+++ src/Pages/AllData.js
@@ -4,14 +4,17 @@ import TableData from '../assets/Jsons/TableData.json'
 const AllData = () => {
     const monthName = ['APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUEST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER', 'JANUARY', 'FEBUARY', 'MARCH']
     return (
-        <div className=' p-10'>
-            <div className="row flex mt-10" >
+        <div className='p-10'>
+        <div className='h-[900px] overflow-scroll p-0'>
+            <div className="row flex  " >
                 <div className="column1 border-2 w-44 p-2"></div>
-                <div className="flex"> {monthName.map((month) => (
-                    <div className="column2 border-2 w-28 p-2 font-bold text-center">{month}</div>
-                ))}</div>
+                <div className="flex">
+                    {monthName.map((month) => (
+                        <div className="column2 border-2 w-28 p-2 font-bold text-center">{month}</div>
+                    ))}</div>
             </div>
             <Columns Data={TableData.form_fields} font={'text-2xl'} />
+        </div>
         </div>
     )
 }
